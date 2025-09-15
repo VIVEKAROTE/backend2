fd4e1ecf04fc72bc175a0bb9178311e92625561e
Vivek Arote <vivekarote28@gmail.com>
2025-09-15 09:00:25 +0530

    util code discussion (Custom api response and error handling)

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode, data, meessage = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = this.message
+        this.success = this.success < 400
+    }
+}
